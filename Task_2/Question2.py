a62b1d5b9d5c6427a4ca98efb455f54d0bd98106
FlrMh <florina.mihai110@gmail.com>
2023-01-31 16:09:03 +0000

    question 2

---
+++ Task_2/Question2.py
@@ -0,0 +1,13 @@
+# Guessing game
+
+# Ask user for a number;
+# If the number is odd - print(The number is odd!)
+# If the number is even - print(The number is even!)
+
+number_choice = int(input("Please, type a number."))
+
+
+if (number_choice % 2) == 0:
+    print("This number is even!")
+else:
+    print("This number is odd!")
