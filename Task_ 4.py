d2a921d7090176c3fa734cc4e9c678effea2248e
FlrMh <florina.mihai110@gmail.com>
2023-02-01 08:37:06 +0000

    task 4

---
+++ Task_ 4.py
@@ -0,0 +1,39 @@
+# Task 4
+
+# Question 1
+
+# x = input ("Please add a number:")
+# x = int (x)
+# total_number = x
+#
+# sum = 0
+# while x >= 0:
+#     sum += x
+#     x -= 1
+#
+# average = sum / total_number
+# print("Sum of", total_number , "using while loop", sum)
+# print("average of", total_number, "using while loop", average)
+
+
+# Question 2:
+
+# x = 300
+#
+# while x < 301:
+#     print (x)
+#     x += 10
+# print(x)
+
+
+print("Factors of 10:")
+x = 0
+
+while x <= 300:
+    print(x, end=" ")
+    x = x + 10
+
+
+
+
+
