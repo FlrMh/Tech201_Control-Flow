ee79610cae8e9fa01a5f744e8a89aad7ded29604
FlrMh <florina.mihai110@gmail.com>
2023-02-01 20:43:40 +0000

    update3

--- Task_1/Task_1_V2.py
+++ Task_1/Task_1_V2.py
@@ -23,24 +23,46 @@
 
 # Define your functions
 
-def over_18(age):
-    print(f"Your age is {age}, therefore, you can watch -A18- and -Universal- rated movie.")
+# def over_18(age):
+#     print(f"Your age is {age}, therefore, you can watch -A18- and -Universal- rated movie.")
+#
+# def between_18_15(age):
+#     print(f"Your age is {age}, therefore, you can watch -15-, -12A-, -12- and -Universal- rated movies.")
+#
+# def between_12_15(age):
+#     print(f"Your age is {age}, therefore, you can watch -12A-, -12-, and -Universal- rated movies.")
+#
+# def under_12(age):
+#     print(f"Your age is {age}, therefore, you can watch -Universal- rated movies.")
+#
+# age = int(input("How old are you? "))
+# if age >= 18:
+#     print(over_18(age))
+# elif age >= 15 and age < 18:
+#     print(between_18_15(age))
+# elif age >=12 and age < 15:
+#     print(between_12_15(age))
+# else:
+#     print(under_12(age))
 
-def between_18_15(age):
-    print(f"Your age is {age}, therefore, you can watch -15-, -12A-, -12- and -Universal- rated movies.")
 
-def between_12_15(age):
-    print(f"Your age is {age}, therefore, you can watch -12A-, -12-, and -Universal- rated movies.")
+# Attempt #2
 
-def under_12(age):
-    print(f"Your age is {age}, therefore, you can watch -Universal- rated movies.")
+# Creating our function:
 
-age = int(input("How old are you? "))
-if age >= 18:
-    print(over_18(age))
-elif age >= 15 and age < 18:
-    print(between_18_15(age))
-elif age >=12 and age < 15:
-    print(between_12_15(age))
-else:
-    print(under_12(age))
+def check_age(age):
+    if age.isdigit() and -1 < int(age) < 117:
+        if int(age) < 12:
+            return "You can watch Universal and PG rated movies."
+        elif 12 <= int(age) < 15:
+            return "You can watch 12, 12A and Universal rated movies."
+        elif 14 < int(age) < 18:
+            return "You can watch Universal, 12, 12A and 15 rated movies."
+        else:
+            return "You can watch all types of movies."
+    else:
+        age = input("What is your age? Please, introduce it in digits.")
+        check_age(age)
+
+age = input("What is your age?")
+print(check_age(age))
