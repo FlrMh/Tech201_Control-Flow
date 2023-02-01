ffd427af502bd30b9f40a26d6a43c0d91093efa3
FlrMh <florina.mihai110@gmail.com>
2023-02-01 11:32:00 +0000

    showcase

---
+++ ten_integers_showcase.py
@@ -0,0 +1,22 @@
+# while loops
+
+# amount = 0 # amount of inputs the user made
+# number = 0 # number they input
+# # we set the numbers to 0 because it`s the tracker - they track the number of inputs made by the user, and track the numbers added and adds them up
+# # logically, it makes sense to use 0 as a tracker if you want to track numbers
+# while True:
+#     number += int(input("Add a number: "))
+#     amount += 1
+#     if amount == 10:
+#         break
+#
+# print(f"Numbers in total: {amount}.")
+# print(f"Sum of numbers: {number}")
+# print(f"The average of your numbers is: {number/10}.")
+
+# For loops
+
+# total = 0 # will track the sum of numbers
+# for i in range(10):
+#     total += int(input("Add a number: "))
+# print(f"The total sum of your numbers is: {total}.")
